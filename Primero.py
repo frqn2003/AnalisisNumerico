2bd018821f41fc20a5e9c89272f4335ca0c8a732
frqn2003 <161607173+frqn2003@users.noreply.github.com>
2025-04-18 14:54:58 -0300

    Delete Primero.py

--- Primero.py
+++
@@ -1,3 +0,0 @@
-# hola, fui creando la funcion validar con gpt, con eso y la funcion.txt podemos ingresar directamente la funcion 
-# dentro del txt como string, lo cargamos y lo valida, sympy simplifica la funcion, falta hacer 
-#la parte grafica que cargue el string en el .txt y que luego lo lea. 
